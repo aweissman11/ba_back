22b53668cc9e9fd33c7c620f5aec2d0e455d05e3
Aaron Weissman <weissman.aaron@gmail.com>
2020-01-05 18:11:31 -0700

    working, but missing routes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -22,14 +22,14 @@ app.use(cors());
 app.use(compression());
 app.use(bodyParser.json());
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,127 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const _ = require('underscore');
+const request = require("request");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const uuidv4 = require('uuid/v4');
+const jwtDecode = require('jwt-decode');
+
+const router = express.Router();
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+
+const tableName = 'ba_rsvps';
+let user_id = 'test_user';
+let user_name = 'Test User';
+
+// /* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'BA Back' });
+});
+
+router.post('/api/rsvp', (req, res, next) => {
+  let item = req.body.Rsvp;
+  item.user_id = user_id;
+  item.user_name = user_name;
+  item.unid = user_id + ':' + uuidv4();
+  item.timestamp = Date.now().toString();
+
+  docClient.put({
+    TableName: tableName,
+    Item: item
+  }, (err, data) => {
+    if (err) {
+      console.log('err :', err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      })
+    } else {
+      return res.status(200).send(item);
+    }
+  })
+});
+
+router.patch('/api/rsvp', (req, res, next) => {
+  let item = req.body.Rsvp;
+  item.user_id = user_id;
+  item.user_name = user_name;
+
+  docClient.put({
+    TableName: tableName,
+    Item: item,
+    ConditionExpression: '#t = :t',
+    ExpressionAttributeNames: {
+      '#t': 'unid'
+    },
+    ExpressionAttributeValues: {
+      ':t': item.unid
+    }
+  }, (err, data) => {
+    if (err) {
+      console.log('err :', err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    } else {
+      return res.status(200).send(item);
+    }
+  })
+});
+
+router.get('/api/rsvps', (req, res, next) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 5;
+  let params = {
+    TableName: tableName,
+    KeyConditionExpression: "user_id = :uid",
+    ExpressionAttributeValues: {
+      ":uid": user_id
+    },
+    Limit: limit,
+    ScanIndexForward: false
+  };
+
+  let startTimestamp = req.query.start ? parseInt(req.query.start) : 0;
+
+  if (startTimestamp > 0) {
+    params.ExclusiveStartKey = {
+      user_id: user_id,
+      timestamp: startTimestamp
+    }
+  }
+
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    } else {
+      return res.status(200).send(data);
+    }
+  });
 });
 
 module.exports = router;
+
+
+
+// let blankInfo = {
+//   "name": "",
+//   "email": "",
+//   "firstTime": false,
+//   "people": [],
+//   "lodging": "tent",
+//   "dogs": "",
+//   "arrival": "June 26, 2020",
+//   "events": [],
+//   "chores": [],
+//   "driving": "full",
+//   "spots": "",
+//   "songs": []
+// }
