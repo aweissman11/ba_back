cdf4f7518e2f9f79c6e981ae8dc222bb171b7ee7
Aaron Weissman <weissman.aaron@gmail.com>
2020-03-10 10:43:20 -0600

    better error handling

--- routes/utils/helpers.js
+++ routes/utils/helpers.js
@@ -3,8 +3,7 @@ const validateFields = (item, isPatch) => {
   let requiredFields = [
     'name', 'email', 'firstTime',
     'people', 'lodging', 'dogs',
-    'arrival', 'events', 'chores',
-    'driving', 'spots', 'songs',
+    'arrival', 'events', 'songs',
     'user_name', 'user_id', 'attending'
   ];
 
@@ -16,30 +15,51 @@ const validateFields = (item, isPatch) => {
 
   if (missingProps.length) {
     return {
-      message: `missing required fields: ${missingProps}`,
+      message: `Application error. Missing fields: ${missingProps.join(', ')}`,
       status: 400,
       missingProps
     };
   }
 
-
-  let stringFields = [
-    'name', 'email', 'dogs',
-    'spots', 'user_name', 'user_id'
+  let requiredStrings = [
+    'name', 'email'
   ]
 
-  let emptyProps = stringFields.filter(field => {
-    return item[field].length === 0;
-  });
+  let niceStrings = {
+    name: 'Your Name',
+    email: 'Email Address'
+  }
+
+  let noBlanks = []
+  requiredStrings.forEach(string => {
+    if (!item[string].length) {
+      noBlanks.push(niceStrings[string])
+    }
+  })
+
+  if (noBlanks.length) {
+    return {
+      message: `The following fields cannot be left blank: ${noBlanks.join(', ')}`,
+      status: 400,
+      noBlanks
+    }
+  }
+
+  let emptySubProps = []
 
   let attendeeFields = [
     'fullName', 'allergies'
   ];
 
+  let niceAttendee = {
+    fullName: 'Full Name',
+    allergies: 'Allergies'
+  }
+
   attendeeFields.forEach(field => {
     item.people.forEach(person => {
       if (!person[field].length) {
-        emptyProps.push('Attendee ' + field);
+        emptySubProps.push(`Attendee's ` + niceAttendee[field]);
       }
     })
   })
@@ -48,24 +68,52 @@ const validateFields = (item, isPatch) => {
     'song', 'artist'
   ];
 
+  let niceSong = {
+    song: 'Title',
+    artist: 'Artist'
+  }
+
   songFields.forEach(field => {
     item.songs.forEach(song => {
       if (!song[field].length) {
-        field = field === 'song' ? 'name' : field;
-        emptyProps.push('Song ' + field);
+        emptySubProps.push('Song ' + niceSong[field]);
       }
     })
   })
 
-  if (emptyProps.length) {
+  if (emptySubProps.length) {
     return {
-      message: `${emptyProps} cannot be left blank`,
+      message: `The following fields cannot be left blank: ${emptySubProps.join(', ')}`,
       status: 400,
       ok: false,
-      emptyProps
+      emptySubProps
     };
   }
 
+  // ==========================================
+  // MAKE SURE THEY SIGN UP FOR EVENTS AND ADD ATTENDEES
+  // ==========================================
+
+  if (!item.people.length) {
+    return {
+      message: `No one is coming? Not even you? Please click the 'Add Attendee' button and let us know who we need to feed.`,
+      status: 400,
+      ok: false
+    };
+  }
+
+  if (!item.events.length) {
+    return {
+      message: `You're really not coming to any events? Please click the 'RSVP for Events' button and let us know which ones you'll be attending.`,
+      status: 400,
+      ok: false
+    };
+  }
+
+
+  // ==========================================
+  // EMAIL VALIDATION
+  // ==========================================
   const validateEmail = (email) => {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
